0b139bcb5a267652b88c54c5e2a23ba256a42932
wahello <wahello@users.noreply.github.com>
2024-12-14 06:21:20 +0000

    deploy: 08dfeb1ff15d1f65935761ae5d75376b384433e9

---
+++ sui_data_ingestion/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["ArchivalConfig","ArchivalReducer","ArchivalWorker","BlobTaskConfig","BlobWorker","IngestionWorkflowsProgressStore","KVStoreTaskConfig","KVStoreWorker"]};
